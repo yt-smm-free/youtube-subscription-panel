8b76038cadbf9ba13eda17346b1aaff23426408f
yt-smm-free <desiengineer28@gmail.com>
2025-08-07 20:20:33 +0000

    Save local changes before pull

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ mongoose.connect(process.env.MONGODB_URI)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(morgan('dev'));
+app.use(morgan('production'));
 app.use(helmet({
   contentSecurityPolicy: {
     directives: {
